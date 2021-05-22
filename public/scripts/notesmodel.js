45d65f99bc9ef81bc947bf5c7176b730bf92a23d
Caro <carole.hug@outlook.com>
2021-05-22 18:54:16 +0200

    add fonts

--- public/scripts/notesmodel.js
+++ public/scripts/notesmodel.js
@@ -1,7 +1,13 @@
+function getTodaysDate() {
+    const dateTodayUnformatted = new Date();
+    const test = `${dateTodayUnformatted.getDay().toString()}.${dateTodayUnformatted.getMonth().toString()}.${dateTodayUnformatted.getFullYear().toString()}`;
+    return test;
+}
+
 const notes = [
     {id: '01', title: 'Mami anrufen', endDate: '', finished: false, finishDate: '', description: '888 888 88 88', urcengy: 0},
-    {id: '02', title: 'Einkaufen', endDate: new Date().toString(), finished: false, finishDate: '', description: 'Butter, Eier', urcengy: 1},
-    {id: '03', title: 'CAS FEE Selbststudium/Projekt Aufgabe erledigen', endDate: '', finished: true, finishDate: new Date().toString(), description: '888 888 88 88', urcengy: 2},
+    {id: '02', title: 'Einkaufen', endDate: getTodaysDate(), finished: false, finishDate: '', description: 'Butter, Eier', urcengy: 1},
+    {id: '03', title: 'CAS FEE Selbststudium/Projekt Aufgabe erledigen', endDate: '', finished: true, finishDate: getTodaysDate().toString(), description: 'HTML für die note App erstellen, CSS erstellen für die note App', urcengy: 2},
 ];
 
 function compareNotes(s1, s2) {
@@ -11,3 +17,6 @@ function compareNotes(s1, s2) {
 function notesSorted() {
     return [...notes].sort(compareNotes);
 }
+
+
+
