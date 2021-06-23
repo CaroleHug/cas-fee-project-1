c4c0e5f08c262b9a655066d7fe5d4eaa730e3c0a
Caro <carole.hug@outlook.com>
2021-06-23 21:29:36 +0200

    add images for priority

--- public/controllers/note-controller.js
+++ public/controllers/note-controller.js
@@ -24,8 +24,11 @@ export class NoteController {
             this.newNote = document.getElementById('newNote');
             this.newNoteSubmit = document.getElementById('new-note-submit');
             this.cancel = document.getElementById('cancel');
+            this.priorityRadios = document.getElementsByName('priority');
+
             this.setCurrentFormValues();
             this.readUrlParams();
+            this.addPriorityRadioEventListeners();
         }
     }
 
@@ -50,10 +53,6 @@ export class NoteController {
                 this.getNotes(this.selectedOrder, this.showFinishedChecked);
             });
 
-            // this.noteContainer.addEventListener('click', (event) => {
-            //     const noteId = Number(event.target.dataset.id);
-            // });
-
             this.orderCriterias.forEach((radio) => {
                 radio.addEventListener('click', () => {
                     this.selectedOrder = radio.value;
@@ -70,7 +69,7 @@ export class NoteController {
             this.newNoteSubmit.addEventListener('click', (event) => {
                 this.setCurrentFormValues();
                 if (this.newNote.reportValidity()) {
-                    noteService.submitNote(new Note(this.id, this.newTitle, this.newDescription, this.newPriority, this.selectedFinished || false, this.newEndDate)); //TODO remove hardcoded elements
+                    noteService.submitNote(new Note(this.id, this.newTitle, this.newDescription, this.newPriority, this.selectedFinished || false, this.newEndDate));
                     window.location.href = `http://localhost:3000/index.html?theme=${this.selectedTheme}`;
                 }
                 event.preventDefault();
@@ -93,12 +92,26 @@ export class NoteController {
 
         this.finishedCheckboxes.forEach((checkbox) => {
             checkbox.addEventListener('click', () => {
-                console.log(this.notes);
                 const checkedNote = this.notes.find((note) => note._id === checkbox.value);
-                console.log(checkbox.checked);
                 const checkedNoteFinished = checkbox.checked;
                 noteService.submitNote(new Note(checkedNote._id, checkedNote.title, checkedNote.description, checkedNote.priority, checkedNoteFinished, checkedNote.endDate));
                 this.getNotes(this.selectedOrder, this.showFinishedChecked);
+            });
+        });
+    }
+
+    addPriorityRadioEventListeners() {
+        this.priorityRadios.forEach((radio) => {
+            radio.addEventListener('click', () => {
+                this.priorityRadios.forEach((element) => {
+                    element.setAttribute('src', './data/flash-grey.svg');
+                });
+
+                let i = 0;
+                while (this.priorityRadios[i - 1] !== radio) {
+                    this.priorityRadios[i].setAttribute('src', './data/flash-yellow.svg');
+                    i++;
+                }
             });
         });
     }
@@ -137,7 +150,7 @@ export class NoteController {
         this.newTitle = document.getElementById('title').value;
         this.newDescription = document.getElementById('description').value;
         this.newEndDate = document.getElementById('endDate').value;
-        const priority = [...document.getElementsByName('priority')].filter((item) => item.checked)[0];
+        const priority = [...document.getElementsByName('priority-radio')].filter((item) => item.checked)[0];
         if (priority) {
             this.newPriority = priority.value;
         }
